7769ea29786137180bf7d2ad742db4eaf742764b
lechien324111-sys <lechien324111@gmail.com>
2026-05-17 16:36:53 +0700

    Add files via upload

---
+++ script.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Bypass UptoLink (NekoVCheat)
+// @namespace    https://vanhcheat.rf.gd
+// @version      2.0
+// @description  Bypass UptoLink — host trên vanhcheat.rf.gd
+// @require      https://raw.githubusercontent.com/Asustv123/Bypass-uptolink1/main/source.js
+// @match        *://*/*
+// @author       NekoVCheat
+// @grant        GM_xmlhttpRequest
+// @connect      uptolink.one
+// @connect      vanhcheat.rf.gd
+// @connect      api.kolosal.ai
+// @connect      *
+// @run-at       document-end
+// ==/UserScript==
